1074a2ef08fe89d5563156f0f5f0a597e9d77e79
lesha.pros.1998@bk.ru <dark.revo4@mail.ru>
2018-02-09 06:39:57 -0700

    del

--- app/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-import '../blocks/main-menu/main-menu.js'
-import '../blocks/search-button/search-button.js'
-import '../blocks/slider/slider.js'
-
-$(() => {
-    svg4everybody();
-});
